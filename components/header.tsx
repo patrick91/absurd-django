e37bd7c4c717ec25fa64d84a58675a1e2711a4f6
Patrick Arminio <patrick.arminio@gmail.com>
2022-09-24 16:22:32 +0100

    Fix

--- components/header.tsx
+++ components/header.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="bg-indigo-600">
+    <header className="bg-green-600">
       <nav className="px-4" aria-label="Top">
-        <div className="w-full py-2 flex items-center justify-between border-b border-indigo-500 lg:border-none">
+        <div className="w-full py-2 flex items-center justify-between border-b border-green-500 lg:border-none">
           <div className="flex items-center">
             <Link href="/">
-              <a className="font-bold text-white">Diagrams web</a>
+              <a className="font-bold text-white">Absurd django</a>
             </Link>
           </div>
           <div className="ml-10 space-x-4">
             <a
-              href="https://github.com/patrick91/diagrams-web"
-              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-sm font-medium text-indigo-600 hover:bg-indigo-50"
+              href="https://github.com/patrick91/absurd-django"
+              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-sm font-medium text-green-600 hover:bg-green-50"
             >
               GitHub
             </a>
