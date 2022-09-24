6b4300aa9b874510f24c5baf4991b5ef6808837d
Wilhelm Klopp <wil.klopp@gmail.com>
2022-09-24 16:35:09 +0100

    Use django-green color

--- components/header.tsx
+++ components/header.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="bg-green-600">
+    <header className="bg-django-green">
       <nav className="px-4" aria-label="Top">
-        <div className="w-full py-2 flex items-center justify-between border-b border-green-500 lg:border-none">
+        <div className="w-full py-2 flex items-center justify-between border-b border-django-green lg:border-none">
           <div className="flex items-center">
             <Link href="/">
               <a className="font-bold text-white">Absurd django</a>
@@ -13,7 +13,7 @@ export const Header = () => {
           <div className="ml-10 space-x-4">
             <a
               href="https://github.com/patrick91/absurd-django"
-              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-sm font-medium text-green-600 hover:bg-green-50"
+              className="inline-block bg-white py-2 px-4 border border-transparent rounded-md text-sm font-medium text-django-green hover:bg-green-50"
             >
               GitHub
             </a>
