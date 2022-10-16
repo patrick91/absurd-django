7afeab09ab44a8c91d8d5039f1bcb2b176ef3d46
Patrick Arminio <patrick.arminio@gmail.com>
2022-10-16 15:36:25 -0700

    Move default code to a file

--- components/editor-with-preview.tsx
+++ components/editor-with-preview.tsx
@@ -7,81 +7,10 @@ import { useState, useMemo, useEffect, useCallback } from "react";
 import { PyodideProvider, usePyodide } from "./pyodide";
 import { POST_CODE, SETUP_CODE } from "../lib/django";
 
-const DEFAULT_CODE = `
-from django.db import models
-from django.db import connection
-from django.template import Template, Context
-from django.http import HttpResponse, HttpResponseRedirect
+// @ts-ignore
+import code from "../lib/default_code.py";
 
-TEMPLATE = """
-<h1>Add new todo</h1>
-
-<form action="/add-todo" method="post">
-    <input type="text" name="text" />
-    <button>Add</button>
-</form>
-
-<h1>My Todos</h1>
-
-<ul>
-   {% for todo in todos %}
-   <li>{{ todo.text }}</li>
-   {% endfor %}
-</ul>
-"""
-
-import random
-
-from pathlib import Path
-
-def _create_db():
-    # TODO: create db automatically from Django models
-    with connection.cursor() as cursor:
-        cursor.execute("""
-          create table if not exists abc_todo (id INTEGER PRIMARY KEY, text STRING);
-        """)
-
-
-class Todo(models.Model):
-    text = models.TextField()
-
-    class Meta:
-        app_label = "abc"
-
-def add_todo(request):
-    _create_db()
-
-    # TODO: raise if it is not post
-
-    text = request.POST["text"]
-
-    todo = Todo.objects.create(text=text)
-
-    return HttpResponseRedirect("/")
-
-def todos(request):
-    _create_db()
-
-    todos = Todo.objects.all().order_by("-id")
-
-    t = Template(TEMPLATE)
-    c = Context({"todos": todos})
-    return HttpResponse(t.render(c))
-
-def clear(request):
-    _create_db()
-
-    Todo.objects.all().delete()
-
-    return HttpResponse("Cleared all todos")
-
-
-urlpatterns = [
-    path("", todos),
-    path("add-todo", add_todo),
-    path("clear", clear),
-]
-`.trim();
+const DEFAULT_CODE = code;
 
 const getCode = (code: string) => {
   return [SETUP_CODE, code, POST_CODE].join("\n");
